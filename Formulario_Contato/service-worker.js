f9aef86fbe1c2d375601712481bbc65bb75dd4ac
Alexandre Machado <156339431+alexandreM12@users.noreply.github.com>
2024-09-11 23:08:37 -0300

    Add files via upload

---
+++ Formulario_Contato/service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "formulario-contato-v1";
+const urlsToCache = [
+  "/",
+  "/styles.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css",
+  "/imag/document_icon_246433.png",
+  "/index.html"
+];
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", function(event) {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
